cb9548a7aa78842a056c34b63aa27af5130e8278
Victorccding <82922344+Victorccding@users.noreply.github.com>
2021-11-23 11:14:55 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,20 +2,7 @@ module.exports = {
   images: {
     domains: ["picsum.photos"],
     formats: ['image/avif', 'image/webp'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|png)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=120, must-revalidate',
-          }
-        ],
-      },
-    ]
+    minimumCacheTTL: 60,
   },
 };
 
